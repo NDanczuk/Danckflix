c86b01af7892ecfa0683a820cba72a53487bc1cd
NDanczuk <nicolasdanc@gmail.com>
2025-01-15 10:20:35 -0300

    add home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
+import { Link } from 'react-router-dom'
+import './home.css'
+
 export default function Home(){
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function loadMovies(){
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: import.meta.env.VITE_MOVIEDB_API_KEY,
+                    page: 1
+                }
+            })
+
+            setMovies(response.data.results.slice(0, 10))
+            setLoading(false)
+        }
+
+        loadMovies()
+    })
+
+    if(loading){
+        return(
+            <div className='loading'>
+                <h2>Loading movies...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Welcome!</h1>
+        <div className='container'>
+            <div className='movies-list'>
+                {movies.map((movie) => {
+                    return(
+                        <article key={movie.id}>
+                            <strong>{movie.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}/>
+                            <Link to={`/movie/${movie.id}`}>See movie</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
